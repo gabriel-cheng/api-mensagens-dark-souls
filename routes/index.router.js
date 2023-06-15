c9bfb1f27b7889b371161a33cd417ecb3860af89
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2023-06-15 17:26:34 -0300

    feat: adicionando configuração de cors

--- routes/index.router.js
+++ routes/index.router.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controller/index.controller");
+const cors = require("cors");
+
+router.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    router.use(cors());
+    next();
+});
 
 router.get("/", controller.index);
 router.get("/:categoria", controller.categoria);
